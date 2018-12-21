6995e213d5e01600acf4fc5b9347d4111ba54d09
goralive <nosovskyigor@gmail.com>
2018-12-21 17:52:52 +0200

    Two lines are created in rectangle

--- src/rectangle/Rectangle.java
+++ src/rectangle/Rectangle.java
@@ -1,12 +1,24 @@
 package rectangle;
 
 
+import java.util.Scanner;
+
 public class Rectangle {
     public static void main(String[] args) {
 
-        for (int i = 0; i < 4; i++) {
-            for (int j = 0; j < 4; j++) {
-                System.out.print("* ");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter value for rectangle: ");
+
+        int userValue = Integer.valueOf(scanner.nextLine());
+
+        for (int i = 0; i < userValue; i++) {
+            for (int j = 0; j < userValue; j++) {
+                if (i == 0 || i == userValue - 1) {
+                    System.out.print("* ");
+                } else {
+                    System.out.print(" ");
+                }
+
             }
             System.out.println();
         }
