1619558bc9191daf18514d5edba2ad82cd756931
goralive <nosovskyigor@gmail.com>
2019-01-04 12:15:40 +0200

    Add constant

--- src/javaStart/taxes/Parser.java
+++ src/javaStart/taxes/Parser.java
@@ -4,12 +4,13 @@ public class Parser {
 
 
     public static long doubToLong(double number) {
-        long temp = (long) (number);
+        long temp = (long) number * QuaterTaxes.CALCULATION_CONSTANT;
         return temp;
     }
 
     public static double longToDoub(long number) {
-        double temp = (double) (number);
+
+        double temp = (double) (number / QuaterTaxes.CALCULATION_CONSTANT);
         return temp;
     }
 }
--- src/javaStart/taxes/QuaterTaxes.java
+++ src/javaStart/taxes/QuaterTaxes.java
@@ -3,6 +3,7 @@ package javaStart.taxes;
 // taxes. Get user input float for course value, * 0.95 (5%) + 820 ghr (ESV) * 3 months (quarter)
 public class QuaterTaxes {
     static long tax = 0;
+    static final int CALCULATION_CONSTANT = 100;
 
     public static void currencySelector(String currency) {
         switch (currency.toLowerCase()) {
@@ -25,18 +26,18 @@ public class QuaterTaxes {
     }
 
     public static long euro(long euroProfit) {
-        System.out.println("Course value is: " + "31.63\n" + "Your profit in EUR is: " + euroProfit);
-        return euroProfit * 3163 / 100;
+        System.out.println("Course value is: " + "31.63\n" + "Your profit in EUR is: " + Parser.longToDoub(euroProfit));
+        return euroProfit * 3163 / CALCULATION_CONSTANT;
     }
 
     public static long dollar(long dollarProfit) {
-        System.out.println("Course value is: " + "27.91\n" + "Your profit in USD is: " + dollarProfit);
-        return dollarProfit * 2791 / 100;
+        System.out.println("Course value is: " + "27.91\n" + "Your profit in USD is: " + Parser.longToDoub(dollarProfit));
+        return dollarProfit * 2791 / CALCULATION_CONSTANT;
     }
 
     public static long hryvnia(long hryvniaProfit) {
-        System.out.println("Your profit in hryvnia: " + hryvniaProfit);
-        tax = (((hryvniaProfit * 100) - (hryvniaProfit * 95)) + 246000) / 100;
+        System.out.println("Your profit in hryvnia: " + Parser.longToDoub(hryvniaProfit));
+        tax = (hryvniaProfit - (hryvniaProfit * 95 / CALCULATION_CONSTANT)) + 246000;
         return tax;
     }
 }
