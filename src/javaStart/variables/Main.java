39898d67c0a7f25bece0a0ca692df004a021b6dc
goralive <nosovskyigor@gmail.com>
2019-01-04 17:41:17 +0200

    Functionality for number on new line

---
+++ src/javaStart/variables/Main.java
@@ -0,0 +1,27 @@
+package javaStart.variables;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Greetings,\n\nHere is the list of the home tasks. Type a number to start:\n");
+        System.out.println("1. Type a number and you receive each number on the line ");
+        System.out.println("2. Triangle square calculation");
+        System.out.println("3. Calculation of the circle width");
+
+        switch (UserInput.returnInt()) {
+            case 1:
+                System.out.println("Please enter 5 digits");
+                ReadNumberAndType.number(UserInput.returnLine());
+                break;
+            case 2:
+                //TriangleSquare
+                break;
+            case 3:
+                //CircleWidth
+            default:
+                System.out.println("Select program using number from 1-3");
+        }
+
+    }
+}
---
+++ src/javaStart/variables/ReadNumberAndType.java
@@ -0,0 +1,9 @@
+package javaStart.variables;
+
+//page 64/1
+public class ReadNumberAndType {
+    public static void number(String number) {
+
+
+    }
+}
