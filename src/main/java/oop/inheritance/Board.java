1fcfd15707712ffca75300ed9d7e113003250d70
igor.nosovskyi <nosovskyigor@gmail.com>
2019-02-05 18:39:06 +0100

    Add board and methods. Not finish!

--- src/main/java/oop/inheritance/Board.java
+++ src/main/java/oop/inheritance/Board.java
@@ -1,25 +1,29 @@
 package oop.inheritance;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class Board {
-    private Map<Integer,Shape> boardSize = new HashMap<Integer, Shape>(4);
 
-    public static void main(String[] args) {
+    private List<Shape> board = new ArrayList<Shape>(4);
 
+
+    public void addFigureToBoard (Shape shapeOfFigure) {
+        board.add(shapeOfFigure);
     }
-    public void addFigureToBoard () {
 
-    }
-
-    public void deleteFigureFromBoard () {
-
+    public void deleteFigureFromBoard (int numberOfDeleteFigure) {
+        board.remove(numberOfDeleteFigure);
     }
 
     public void printInformationFigureOnTheBoard () {
-
+        System.out.println("On the board you can see: ");
+        for (Shape shape : board) {
+            //shape.
+        }
     }
 
     public void overalSqueareOfTheFiguresOnTheBoard () {
--- src/main/java/oop/inheritance/Square.java
+++ src/main/java/oop/inheritance/Square.java
@@ -1,4 +1,13 @@
 package oop.inheritance;
 
-public class Square {
+public class Square extends Shape {
+    @Override
+    double getPerimeter() {
+        return 0;
+    }
+
+    @Override
+    double getArea() {
+        return 0;
+    }
 }
