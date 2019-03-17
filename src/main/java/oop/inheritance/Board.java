5627c161f1a4b81f9cd54291d1e5f6d6973f30b7
goralive <nosovskyigor@gmail.com>
2019-03-17 20:49:12 +0200

    Add point to figures

--- src/main/java/oop/inheritance/Board.java
+++ src/main/java/oop/inheritance/Board.java
@@ -1,10 +1,10 @@
 package oop.inheritance;
 
-import java.lang.reflect.Array;
+
 import java.util.ArrayList;
-import java.util.HashMap;
+
 import java.util.List;
-import java.util.Map;
+
 
 public class Board {
 
--- src/main/java/oop/inheritance/Triangle.java
+++ src/main/java/oop/inheritance/Triangle.java
@@ -1,49 +1,16 @@
 package oop.inheritance;
 
 public class Triangle extends Shape {
-    private Point a;
-    private Point b;
-    private Point c;
     private final String name = "Triangle";
 
-    Point pointA = new Point(1,1);
-    Point pointB = new Point(2,2);
-    Point pointC = new Point(2,2);
+   private Point a = new Point(1,1);
+   private Point b = new Point(2,2);
+   private Point c = new Point(2,2);
 
     public Triangle() {
         super();
 
     }
-
-    public String getName() {
-        return name;
-    }
-
-    public Point getA() {
-        return a;
-    }
-
-    public void setA(Point a) {
-        this.a = a;
-    }
-
-    public Point getB() {
-        return b;
-    }
-
-    public void setB(Point b) {
-        this.b = b;
-    }
-
-    public Point getC() {
-        return c;
-    }
-
-    public void setC(Point c) {
-        this.c = c;
-    }
-
-
 
     @Override
     double getPerimeter() {
