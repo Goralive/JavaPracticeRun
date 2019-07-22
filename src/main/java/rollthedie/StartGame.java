a919dc429caa5ac5a6960043d7e15839c72dec5b
Goralive <nosovskyigor@gmail.com>
2019-07-22 21:05:13 +0300

    Add LetsRoll game

---
+++ src/main/java/rollthedie/StartGame.java
@@ -0,0 +1,15 @@
+package rollthedie;
+
+public class StartGame {
+    public static void main(String[] args) {
+        //Roll the die number between 1-6
+        // How many rolls they have to win
+        // Total is 5 rolls
+        //if less then 5 rolls player win
+        // if more then 20 they lose
+        // no more 20
+        LetsRoll letsRoll = new LetsRoll();
+        letsRoll.play();
+
+    }
+}
