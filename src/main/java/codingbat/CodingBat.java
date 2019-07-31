ba10347ac5bb9ecf02cdb0068cd18c6903976a23
Goralive <nosovskyigor@gmail.com>
2019-07-31 18:23:11 +0300

    Add codingbat

---
+++ src/main/java/codingbat/CodingBat.java
@@ -0,0 +1,35 @@
+package codingbat;
+
+public class CodingBat {
+    public String makeTags(String tag, String word) {
+        return "<" + tag + ">" + word + "</" + tag + ">";
+    }
+
+    public String firstTwo(String str) {
+        return 2 < str.length() ? str.substring(0, 2) : str;
+    }
+
+    public String firstHalf(String str) {
+        return str.substring(0,str.length() / 2);
+    }
+
+    public String withoutEnd(String str) {
+        return str.substring(1,str.length()-1);
+    }
+
+    public String comboString(String a, String b) {
+        if (a.length() < b.length()) {
+            return a + b + a;
+        } else {
+            return b + a + b;
+        }
+    }
+
+    public String nonStart(String firstString, String secondString) {
+        return firstString.substring(1)+secondString.substring(1);
+    }
+
+    public String left2(String str) {
+        return str.substring(2).concat(str.substring(0,2));
+    }
+}
