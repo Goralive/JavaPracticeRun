7c8425225240a996b82eb75354be9a8427ad413d
Igor Nosovsky <ignos@ciklum.com>
2019-08-09 18:00:08 +0300

    Refactor

--- src/main/java/passwordvalidator/PasswordValidator.java
+++ src/main/java/passwordvalidator/PasswordValidator.java
@@ -6,27 +6,20 @@ public class PasswordValidator {
     public boolean checks(User createdNewUser, User existingUser) {
         String password = createdNewUser.getPassword();
 
-        if (validPassLength <= password.length()) {
-            if (isUpperCase(password)) {
-                if (isLowerCase(password)) {
-                    if (isSpecialCharacter(password)) {
-                        if (checkPassword(createdNewUser, existingUser)) {
-
-                        }
-                    }
-                } else {
-                    System.out.println("Your password should contain an lowercase letters");
-                    return false;
-                }
-            } else {
-                System.out.println("Your password should contain an uppercase letters");
-                return false;
-            }
-        } else {
-            System.out.println("Your password less then 8 characters");
-            return false;
+        if (passwordLength(createdNewUser) && isUpperCase(password) &&
+                isLowerCase(password) && isSpecialCharacter(password) &&
+                checkPassword(createdNewUser, existingUser) && checkUsername(createdNewUser, existingUser)) {
+            return true;
         }
-        return checkUsername(createdNewUser, existingUser);
+        return false;
+    }
+
+    public boolean passwordLength(User user) {
+        if (validPassLength <= user.getPassword().length()) {
+            return true;
+        }
+        System.out.println("Your password is less then 8 characters");
+        return false;
     }
 
     public boolean checkUsername(User newUser, User existingUser) {
@@ -47,29 +40,28 @@ public class PasswordValidator {
 
 
     public boolean isUpperCase(String password) {
-        boolean isUppercase = false;
+        //  String temp = password.replaceAll("[^A-Za-z0-9]", "");
         char[] chars = password.toCharArray();
         char[] upperCaseChars = password.toUpperCase().toCharArray();
         for (int i = 0; i < chars.length; i++) {
             if (chars[i] == upperCaseChars[i]) {
-                isUppercase = true;
-                break;
+                return true;
             }
         }
-        return isUppercase;
+        System.out.println("Your password should contain an uppercase letters");
+        return false;
     }
 
     public boolean isLowerCase(String password) {
-        boolean isLowerCase = false;
         char[] chars = password.toCharArray();
         char[] lowerCaseChars = password.toLowerCase().toCharArray();
         for (int i = 0; i < chars.length; i++) {
             if (chars[i] == lowerCaseChars[i]) {
-                isLowerCase = true;
-                break;
+                return true;
             }
         }
-        return isLowerCase;
+        System.out.println("Your password should contain an lowercase letters");
+        return false;
     }
 
     public boolean isSpecialCharacter(String password) {
