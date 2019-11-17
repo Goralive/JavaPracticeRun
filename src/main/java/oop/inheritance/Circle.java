e5feb48d44543d701908d2189fb76551f61490a7
goralive <nosovskyigor@gmail.com>
2019-11-17 22:31:59 +0200

    Imp with Points

--- src/main/java/oop/inheritance/Circle.java
+++ src/main/java/oop/inheritance/Circle.java
@@ -1,7 +1,17 @@
 package oop.inheritance;
 
 public class Circle extends Shape {
+    private Point a;
+    private Point b;
     private double radius;
+
+
+    public Circle(Point a, Point b) {
+        this.a = a;
+        this.b = b;
+        radius = a.getDistance(a,b);
+    }
+
 
     public Circle(double radius) {
         this.radius = radius;
@@ -9,11 +19,11 @@ public class Circle extends Shape {
 
     @Override
     public double getPerimeter() {
-        return Math.pow(Math.PI,2) * radius;
+        return Math.pow(Math.PI, 2) * radius;
     }
 
     @Override
     public double getArea() {
-        return Math.PI * Math.pow(radius,2);
+        return Math.PI * Math.pow(radius, 2);
     }
 }
