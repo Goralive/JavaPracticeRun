24b4c450ba25588a810333f5843886496de0a4e8
Igor Nosovskyi <nosovskyigor@gmail.com>
2020-01-30 16:29:06 +0200

    change to static methods

--- src/main/java/fileinteractions/FileInteractions.java
+++ src/main/java/fileinteractions/FileInteractions.java
@@ -4,82 +4,57 @@ import java.io.*;
 import java.util.stream.Collectors;
 
 public class FileInteractions {
-    private File[] fileList;
-    private File fileFolderTo;
-    private File fileFolderFrom;
-    private String[] extensionList = {"txt", "doc"};
+    private static String[] extensionList = {"txt", "doc"};
+    private static MyFileFilter fileFilter = new MyFileFilter(extensionList);
 
-    public FileInteractions(String folderFrom, String folderTo) {
-        this.fileFolderTo = new File(folderTo);
-        this.fileFolderFrom = new File(folderFrom);
-    }
+    public static void copy(String folderFrom, String folderTo) {
+        File fileFolderFrom = new File(folderFrom);
+        File fileFolderTo = new File(folderTo);
 
-    public boolean isDirectoryContainsFiles() {
-        MyFileFilter fileFilter = new MyFileFilter(extensionList);
-        fileList = fileFolderFrom.listFiles(fileFilter);
-        return fileList.length > 0;
-    }
+        File[] fileList = fileFolderFrom.listFiles(fileFilter);
 
-    public void directoryCreated() {
-        this.fileFolderTo.mkdirs();
-    }
-
-    public void copy() {
-        System.out.printf("%nThe file will be copied to %s from %s folder", this.fileFolderTo.getAbsolutePath(), this.fileFolderFrom.getAbsolutePath());
-        for (File fileInTheList : fileList)
-            try (FileInputStream fileInputStream = new FileInputStream(fileInTheList);
-                 FileOutputStream fileOutputStream = new FileOutputStream(fileFolderTo.getAbsolutePath() + "/" + fileInTheList.getName())) {
-                byte[] buffer = new byte[1024];
-                int byteReader;
-                for (; (byteReader = fileInputStream.read(buffer)) > 0; ) {
-                    fileOutputStream.write(buffer, 0, byteReader);
+        if (fileList.length > 0) {
+            fileFolderTo.mkdirs();
+            System.out.printf("%nThe file will be copied to %s from %s folder", fileFolderTo.getAbsolutePath(), fileFolderFrom.getAbsolutePath());
+            for (File fileInTheList : fileList)
+                try (FileInputStream fileInputStream = new FileInputStream(fileInTheList);
+                     FileOutputStream fileOutputStream = new FileOutputStream(fileFolderTo.getAbsolutePath() + "/" + fileInTheList.getName())) {
+                    byte[] buffer = new byte[1024];
+                    int byteReader;
+                    for (; (byteReader = fileInputStream.read(buffer)) > 0; ) {
+                        fileOutputStream.write(buffer, 0, byteReader);
+                    }
+                } catch (IOException e) {
+                    System.out.println(e);
                 }
+        }
+    }
+
+    public static void mergeFiles(String folderPath) {
+        File mergedFile = new File(folderPath + "merged.txt");
+        File folder = new File(folderPath);
+        String mergedContent = "";
+
+        File[] listFilesForMerge = folder.listFiles(fileFilter);
+
+        if (listFilesForMerge.length > 0) {
+            for (File file : listFilesForMerge) {
+                try (BufferedReader bufferedReader = new BufferedReader(new FileReader(file))) {
+                    mergedContent += bufferedReader.lines().collect(Collectors.joining(System.lineSeparator())) + "\n";
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            try (BufferedWriter writer = new BufferedWriter(new FileWriter(mergedFile))) {
+                writer.write(mergedContent);
+                writer.flush();
             } catch (IOException e) {
-                System.out.println(e);
-            }
-    }
-
-    public void copyDataFromFile() {
-        if (isDirectoryContainsFiles()) {
-            directoryCreated();
-            copy();
-        } else {
-            System.out.println("No file found for extension");
-            for (String temp : extensionList) {
-                System.out.println(temp);
+                e.printStackTrace();
             }
         }
-    }
-
-    public void mergeTwoFiles(String firstFile, String secondFile) {
-        String merged = readFileForMerge(firstFile, secondFile);
-        writeToNewFile("merged.txt", merged);
-        System.out.println();
-    }
-
-    private void writeToNewFile(String fileName, String content) {
-        File file = new File(fileName);
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter(file))) {
-            writer.write(content);
-            writer.flush();
-        } catch (IOException ex) {
-            ex.printStackTrace();
-        }
-    }
-
-    public String readFileForMerge(String firstFile, String secondFile) {
-        String mergeTwoFiles = readAllLinesWithStream(firstFile) + "\n" + readAllLinesWithStream(secondFile);
-        return mergeTwoFiles;
-    }
-
-    public String readAllLinesWithStream(String filePath) {
-        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(filePath))) {
-            return bufferedReader.lines().collect(Collectors.joining(System.lineSeparator()));
-        } catch (FileNotFoundException ex) {
-            ex.printStackTrace();
-        } catch (IOException ex) {
-            ex.printStackTrace();
-        }
-        return null;
     }
 }
+
--- src/main/java/fileinteractions/Main.java
+++ src/main/java/fileinteractions/Main.java
@@ -2,9 +2,8 @@ package fileinteractions;
 
 public class Main {
     public static void main(String[] args) {
-        FileInteractions fi = new FileInteractions("build/","build/tmp1");
-        fi.copyDataFromFile();
-        fi.mergeTwoFiles("build/copy.txt", "build/tmp1.txt");
+        FileInteractions.copy("build/", "build/tmp1");
+        FileInteractions.mergeFiles("build/");
     }
 }
 
